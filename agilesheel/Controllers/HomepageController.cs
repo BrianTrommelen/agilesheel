6c41e400424cd93f8e9e49116951d23e3c838fc0
Bas Jongbloed <basjongbloed@gmail.com>
2021-03-22 15:14:32 +0100

    Contact pagina

--- agilesheel/Controllers/HomepageController.cs
+++ agilesheel/Controllers/HomepageController.cs
@@ -64,92 +64,9 @@ namespace agilesheel.Controllers
             return View(movieViewModel);
         }
 
-
-        //// GET: HomepageController
-        //public async Task<IActionResult> Index()
-        //{
-        //    DateTime end = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 0, 0, 0);
-        //    MovieViewModel movieViewModel = new MovieViewModel()
-        //    {
-        //        Movies = await _context.Movies.ToListAsync(),
-        //        Shows = await _context.Shows
-        //        .Include(s => s.Movie)
-        //        .Include(s => s.Theater)
-        //        .Where(s => ((s.StartTime > DateTime.Now) && (s.StartTime < end)))
-        //        .OrderBy(s => s.StartTime)
-        //       .ToListAsync()
-        //    };
-
-        //    return View(movieViewModel);
-        //}
-
-        // GET: HomepageController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Contact()
         {
-            return View();
-        }
-
-        // GET: HomepageController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: HomepageController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomepageController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: HomepageController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomepageController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: HomepageController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return View(_context.Cinemas.FirstOrDefault());
         }
     }
 }
