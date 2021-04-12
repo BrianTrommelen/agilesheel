d275e281a484eabce174623b8609311781094e04
niekvlot <vlotniek@gmail.com>
2021-04-12 21:03:09 +0200

    Added reviews + tests

---
+++ agilesheelTests1/Controllers/MoviesControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Moq;
+using agilesheel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
+
+namespace agilesheel.Controllers.Tests
+{
+    [TestClass()]
+    public class MoviesControllerTests
+    {
+        private readonly StoreDbContext _context;
+
+        [TestMethod]
+        public async Task GetMovieIndex()
+        {
+            // Arrange 
+            var mock = new Mock<StoreDbContext>(_context);
+            var controller = new MoviesController(mock.Object);
+
+            // Act 
+            var result = await controller.IndexAsync("", "") as ViewResult;
+
+            // Assert 
+            Assert.IsNotNull(result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task GetMovieIndexWithParameters()
+        {
+            // Arrange 
+            var mock = new Mock<StoreDbContext>(_context);
+            var controller = new MoviesController(mock.Object);
+
+            // Act 
+            // Get Movies by Genre and is3D
+            var result = await controller.IndexAsync("Action", "true") as ViewResult;
+
+            // Assert 
+            Assert.IsNotNull(result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task GetMovieDetail()
+        {
+            // Arrange
+            var mock = new Mock<StoreDbContext>(_context);
+            var controller = new MoviesController(_context);
+
+            // Act 
+            var result = await controller.Details(1);
+            var okResult = result as OkObjectResult;
+
+            // Assert 
+            var actualConfiguration = okResult.Value as Configuration;
+            Assert.AreEqual("Details", actualConfiguration);
+        }
+    }
+}
---
+++ agilesheelTests1/Controllers/TicketsControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using agilesheel.Models;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace agilesheel.Controllers.Tests
+{
+    [TestClass()]
+    public class TicketsControllerTests
+    {
+        private readonly StoreDbContext _context;
+        private IStoreRepository _repo;
+
+        [TestMethod]
+        public async Task GetTickets()
+        {
+            // Arrange 
+            var mock = new Mock<StoreDbContext>(_context);
+            var controller = new TicketsController(mock.Object, _repo);
+
+            // Act 
+            var result = await controller.Index() as ViewResult;
+
+            // Assert 
+            Assert.IsNotNull(result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task GetTicketById()
+        {
+            // Arrange 
+            var mock = new Mock<StoreDbContext>(_context);
+            var controller = new TicketsController(mock.Object, _repo);
+
+            // Act 
+            var result = await controller.Details(2) as ViewResult;
+
+            // Assert 
+            Assert.IsNotNull(result.ViewName);
+        }
+
+        [TestMethod]
+        public void PriceTest()
+        {
+            // Arrange
+            var actual = 9.00;
+            var movie = _context.Movies.Find(2);
+
+            // Act
+            if (movie.Length <= 120)
+            {
+                actual = actual - 1.50;
+            }
+
+            // Assert
+            Assert.AreEqual(7.50, actual);
+        }
+
+    }
+}
